6a730ea7546d94909e78640d31538158c9a0fdfe
Peter Naudus <uselinux@gmail.com>
2021-02-01 06:53:56 -0500

    Update setup.py
    Fixing typos introduced in the last commit

--- setup.py
+++ setup.py
@@ -7,11 +7,11 @@ here = os.path.abspath(os.path.dirname(__file__))
 requires = [
     'pandas==0.23.4',
     'requests==2.25.1',
-    'bs4>==0.0.1',
+    'bs4==0.0.1',
 ]
 
 test_requirements = [
-    'pytest>=4.0.1'
+    'pytest==4.0.1'
 ]
 
 
